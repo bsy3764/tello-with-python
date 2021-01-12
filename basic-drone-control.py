189ff4888ea85500f2ac6b4c6327119a7861401a
bang <bsy3764@gmail.com>
2021-01-12 22:57:40 +0900

    modify basic, response
    basic, response 파일 수정 작업

--- basic-drone-control.py
+++ basic-drone-control.py
@@ -3,53 +3,57 @@ import socket
 import sys
 import time
 
-logging.basicConfig(level = logging.INFO, stream = sys.stdout)
-logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)  # 로깅 레벨을 설정
+logger = logging.getLogger(__name__)    # getLogger의 인자로는 만들고 싶은 로거 이름을 전달
+
 
 class DroneManager(object):
-	def __init__(self, host_ip='192.168.10.2', host_port=8889,
-					drone_ip='192.168.10.1', drone_port=8889):
-		self.host_ip = host_ip
-		self.host_port = host_port
-		self.drone_ip = drone_ip
-		self.drone_port = drone_port
-		self.drone_address = (drone_ip, drone_port)
-		
-		# 소켓 생성
-		# UDP이므로 SOCK_DGRAM 사용
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
-		
-		# 소켓에 주소, 포트 할당
-		self.socket.bind((self.host_ip, self.host_port))
-		
-		# 데이터 보내기(드론에게 명령어 전달)
-		self.socket.sendto(b'command', self.drone_address)   # 드론을 맨 처음 실행 시, 입력해야 함
-		self.socket.sendto(b'streamon', self.drone_address)    # 드론에서 영상을 스트리밍하기 시작
-		# sendto(b'~~')에서 b를 사용하는 이유
-		# Python에선 문자열이 유니코드이지만 네트워크에서 전송할 때는 바이트 여야하기 때문에
-		# 리터럴(상수)의 경우 'b'for bytes 문자열을 추가
-		
-	def __dell__(self):
-		self.stop()    # stop함수 호출
-	
-	def stop(self):
-		self.socket.close()    # 소켓 연결을 닫고 연결된 리소스를 모두 해제
+    def __init__(self, host_ip='192.168.10.2', host_port=8889,
+                 drone_ip='192.168.10.1', drone_port=8889):
+        self.host_ip = host_ip
+        self.host_port = host_port
+        self.drone_ip = drone_ip
+        self.drone_port = drone_port
+        self.drone_address = (drone_ip, drone_port)
 
-	def send_command(self, command):    # 명령어 전달 하는 함수
-		logger.info({'action' : 'send_command', 'command' : command})    # 전달하는 명령어를 로그로 남기기
-		self.socket.sendto(command.encode('utf-8'), self.drone_address)
+        # 소켓 생성
+        # UDP이므로 SOCK_DGRAM 사용
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-	def takeoff(self):
-		self.send_command('takeoff')    # takeoff란 문자열을 send_command함수에 전달
-		
-	def land(self):
-		self.send_command('land')
+        # 소켓에 주소, 포트 할당
+        self.socket.bind((self.host_ip, self.host_port))    # 튜플 형태로 전달
+
+        # 데이터 보내기(드론에게 명령어 전달)
+        self.socket.sendto(b'command', self.drone_address)  # 드론을 맨 처음 실행 시, 입력해야 함
+        self.socket.sendto(b'streamon', self.drone_address)  # 드론에서 영상을 스트리밍하기 시작
+
+    # sendto(b'~~')에서 b를 사용하는 이유
+    # Python에선 문자열이 유니코드이지만 네트워크에서 전송할 때는 바이트 여야하기 때문에
+    # 리터럴(상수)의 경우 'b'for bytes 문자열을 추가
+
+    # 클래스가 메모리에서 삭제되거나, 삭제하는 것을 잊으면
+    def __dell__(self):
+        self.stop()  # stop함수 호출
+
+    def stop(self):
+        self.socket.close()  # 소켓 연결을 닫고 연결된 리소스를 모두 해제
+
+    def send_command(self, command):  # 명령어 전달 하는 함수
+        logger.info({'action': 'send_command', 'command': command})  # 전달하는 명령어를 로그로 남기기
+        self.socket.sendto(command.encode('utf-8'), self.drone_address) # 소켓에 명령어가 문자열로 오므로 인코딩 필요
+
+    def takeoff(self):
+        self.send_command('takeoff')  # takeoff란 문자열을 send_command함수에 전달
+
+    def land(self):
+        self.send_command('land')
+
 
 # __name__ : 모듈의 이름이 저장되는 변수
-if __name__ == '__main__':    # 해당 파이선 파일이 프로그램의 시작점이 맞는지 판단하는 작업
-	drone_manager = DroneManager()
-	drone_manager.takeoff()
-	
-	time.sleep(5)    # 5초간 대기
-	
-	drone_manager.land()
+if __name__ == '__main__':  # 해당 파이썬 파일이 프로그램의 시작점이 맞는지 판단하는 작업
+    drone_manager = DroneManager()
+    drone_manager.takeoff()
+
+    time.sleep(5)  # 5초간 대기
+
+    drone_manager.land()
--- drone-status-response.py
+++ drone-status-response.py
@@ -4,97 +4,103 @@ import sys
 import time
 import threading
 
-logging.basicConfig(level = logging.INFO, stream = sys.stdout)
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
 class DroneManager(object):
-	def __init__(self, host_ip='192.168.10.2', host_port=8889,
-					drone_ip='192.168.10.1', drone_port=8889):
-		self.host_ip = host_ip
-		self.host_port = host_port
-		self.drone_ip = drone_ip
-		self.drone_port = drone_port
-		self.drone_address = (drone_ip, drone_port)
-		
-		# 소켓 생성
-		# UDP이므로 SOCK_DGRAM 사용
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
-		
-		# 소켓에 주소, 포트 할당
-		self.socket.bind((self.host_ip, self.host_port))
-	
-		# 드론의 상태 메시지
-		self.response = None    # 변수 None으로 초기화
-		self.stop_event = threading.Event()    # 상태 메시지 그만 받기
-		
-		# target에 메서드(함수)의 이름 전달
-		# args에는 튜플이 들어감
-		self._response_thread = threading.Thread(target=self.receive_response, 
-									args=(self.stop_event, ))
-		self._response_thread.start()    # 쓰레드 시작(병렬 처리 시작)
-		
-		# 데이터 보내기(드론에게 명령어 전달)
-		self.send_command('command')
-		self.send_command('streamon')
-		
-	def receive_response(self, stop_event):
-		while not stop_event.is_set():    # stop_event가 이벤트가 오기 전까지 반복
-			try:
-				self.response, ip = self.socket.recvfrom(3000)    # 버퍼 사이즈를 3000
-				logger.info({'action' : 'receive_response', 'response' : self.response})
-			except socket.error as ex:
-				logger.error({'action' : 'receive_response', 'ex' : ex})
-				break
-					
-	def __dell__(self):
-		self.stop()    # stop함수 호출
-	
-	def stop(self):
-		self.stop_event.set()    # 이벤트를 설정
-		
-		# 쓰레드에서 메시지를 보내려는데, 이미 close한 소켓으로 인하여 에러 메시지 발생
-		# 메시지를 다 받을 때 까지는 잠시 대기하게 만들기
-		retry = 0
-		while self._response_thread.isAlive():    # 쓰레드가 살아 있다면
-			time.sleep(0.3)    # 0.3초 대기
-			if retry > 30:   # 0.3초씩 대기를 30번 넘게 한다면
-				break
-			retry += 1
-		
-		self.socket.close()    # 소켓 연결을 닫고 연결된 리소스를 모두 해제
+    def __init__(self, host_ip='192.168.10.2', host_port=8889,
+                 drone_ip='192.168.10.1', drone_port=8889):
+        self.host_ip = host_ip
+        self.host_port = host_port
+        self.drone_ip = drone_ip
+        self.drone_port = drone_port
+        self.drone_address = (drone_ip, drone_port)
 
-	def send_command(self, command):    # 명령어 전달 하는 함수
-		logger.info({'action' : 'send_command', 'command' : command})    # 전달하는 명령어를 로그로 남기기
-		self.socket.sendto(command.encode('utf-8'), self.drone_address)
+        # 소켓 생성
+        # UDP이므로 SOCK_DGRAM 사용
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-		# 메시지를 다 받을 때 까지는 잠시 대기하게 만들기
-		retry = 0
-		while self.response is None:
-			time.sleep(0.3)
-			if retry > 3:
-				break
-			retry += 1
-			
-		if self.response is None:
-			response = None
-		else:
-			response = self.response.decode('utf-8')
-		
-		self.response = None
-		return response
+        # 소켓에 주소, 포트 할당
+        self.socket.bind((self.host_ip, self.host_port))
 
-	def takeoff(self):
-		return self.send_command('takeoff')    # takeoff란 문자열을 send_command함수에 전달
-		
-	def land(self):
-		return self.send_command('land')
+        # 드론의 상태 메시지
+        self.response = None  # 변수 None으로 초기화
+        self.stop_event = threading.Event()  # 상태 메시지를 그만 받을 준비(대기 상태)
+        # 이벤트 객체 생성(default flag = 0)
+        # 하나 이상의 쓰레드가 이벤트를 기다리고 있다가, 누군가가 시그널을 주면 일제히 동작을 시작
+
+        # Thread 객체를 얻음, 함수 및 메서드 실행 방식
+        self._response_thread = threading.Thread(target=self.receive_response,  # target에 메서드(함수)의 이름 전달
+                                                 args=(self.stop_event,))       # args에는 튜플이 들어감
+
+        self._response_thread.start()  # 쓰레드 시작, 병렬처리 시작
+
+        # 데이터 보내기(드론에게 명령어 전달)
+        self.send_command('command')
+        self.send_command('streamon')
+
+    # 드론의 메시지를 받는 함수
+    def receive_response(self, stop_event):
+        while not stop_event.is_set():  # stop_event가 이벤트가 오기 전까지 반복, flag의 상태 반환
+            try:
+                # 소켓으로부터 데이터를 수신하고, 관련된 주소 및 포트 번호를 반환
+                self.response, ip = self.socket.recvfrom(3000)  # 버퍼 사이즈를 3000
+                logger.info({'action': 'receive_response', 'response': self.response})
+            except socket.error as ex:
+                logger.error({'action': 'receive_response', 'ex': ex})
+                break
+
+    def __dell__(self):
+        self.stop()  # stop함수 호출
+
+    def stop(self):
+        self.stop_event.set()  # 이벤트를 설정(flag = 1 로 변경)
+
+        # bad file descriptor 로그 발생
+        # 쓰레드에서 메시지를 보내려는데, 이미 close한 소켓으로 인하여 에러 메시지 발생
+        # 메시지를 다 받을 때 까지는 잠시 대기하게 만들기
+        retry = 0
+        while self._response_thread.isAlive():  # 쓰레드가 살아 있다면
+            time.sleep(0.3)  # 0.3초 대기
+            if retry > 30:  # 0.3초씩 대기를 30번 넘게 한다면
+                break
+            retry += 1
+
+        self.socket.close()  # 소켓 연결을 닫고 연결된 리소스를 모두 해제
+
+    def send_command(self, command):  # 명령어 전달 하는 함수
+        logger.info({'action': 'send_command', 'command': command})  # 전달하는 명령어를 로그로 남기기
+        self.socket.sendto(command.encode('utf-8'), self.drone_address)
+
+        # 메시지를 다 받을 때 까지는 잠시 대기하게 만들기
+        retry = 0
+        while self.response is None:    # is : 변수가 같은 Object(객체)를 가리키면 True
+            time.sleep(0.3)
+            if retry > 3:
+                break
+            retry += 1
+
+        if self.response is None:
+            response = None
+        else:
+            response = self.response.decode('utf-8')
+
+        self.response = None
+        return response
+
+    def takeoff(self):
+        return self.send_command('takeoff')  # takeoff란 문자열을 send_command함수에 전달
+
+    def land(self):
+        return self.send_command('land')
+
 
 # __name__ : 모듈의 이름이 저장되는 변수
-if __name__ == '__main__':    # 해당 파이선 파일이 프로그램의 시작점이 맞는지 판단하는 작업
-	drone_manager = DroneManager()
-	drone_manager.takeoff()
-	
-	time.sleep(10)    # 10초간 대기
-	
-	drone_manager.land()
-	drone_manager.stop()
+if __name__ == '__main__':  # 해당 파이썬 파일이 프로그램의 시작점이 맞는지 판단하는 작업
+    drone_manager = DroneManager()
+    drone_manager.takeoff()
+
+    time.sleep(5)  # 5초간 대기
+
+    drone_manager.land()
+    drone_manager.stop()
