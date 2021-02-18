1ff59391cb7fb1e33c845d363b5de901f35ab98d
bang <bsy3764@gmail.com>
2021-02-18 22:16:29 +0900

    mjpeg study
    study web streaming(mjpeg)

--- drone-djitellopy/droneapp/controllers/server.py
+++ drone-djitellopy/droneapp/controllers/server.py
@@ -3,11 +3,12 @@
 
 import logging
 import config
-from DJITelloPy.djitellopy import tello
+from DJITelloPy.djitellopy.tello import Tello
 from flask import render_template, request, jsonify, Response
+import time
 # jsonify : 사용자가 json data를 내보내도록 제공
 
-from droneapp.models.drone_manager import DroneManager
+# from droneapp.models.drone_manager import DroneManager
 from droneapp.models.manual_control_pygame import FrontEnd
 import droneapp.models.course
 
@@ -32,14 +33,11 @@ def controller():
 def command():
     cmd = request.form.get('command')   # POST로 전달받은 값
     logger.info({'action': 'command', 'cmd': cmd})
-    drone = tello.Tello()
-    # default_speed = drone.set_speed(30)
+    drone = Tello()
     if cmd == 'command':
         drone.connect()
     if cmd == 'streamon':
         drone.streamon()
-    if cmd == 'streamoff':
-        drone.streamoff()
     if cmd == 'takeOff':
         drone.takeoff()
     if cmd == 'land':
@@ -94,9 +92,11 @@ def keyboard_cmd():
 def create_replay():
     print("create replay start")
 
+
 # Generator(제네레이터) : iterator(값을 차례대로 꺼낼 수 있는 객체)를 생성해주는 함수
-def video_generator():
-    drone = tello.Tello()
+# def video_generator():
+def gen_frames():
+    drone = Tello()
     for jpeg in drone.video_jpeg_generator():
         # yield으로 순차 출력
         # M JPEG : 영상을 표현하기 위해 JPEG 이미지(.jpg 와 같은 확장자를 사용)를 시간순에 따라 나열한 방식
@@ -107,11 +107,11 @@ def video_generator():
 
 @app.route('/video/streaming')
 def video_feed():
-    return Response(video_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # 코스를 불러오기
 def get_courses(course_id=None):
-    drone = tello.Tello()
+    drone = Tello()
     courses = droneapp.models.course.get_courses(drone)
     if course_id:
         return courses.get(course_id)
--- drone-djitellopy/droneapp/models/manual_control_pygame.py
+++ drone-djitellopy/droneapp/models/manual_control_pygame.py
@@ -4,22 +4,23 @@ import pygame
 import numpy as np
 import time
 
-# Speed of the drone
-S = 60
-# Frames per second of the pygame window display
-# A low number also results in input lag, as input information is processed once per frame.
+S = 60  # 드론 스피드
+
+# Pygame 창 디스플레이의 초당 프레임 수
+# 입력 정보가 프레임당 한 번 처리되기 때문에 숫자가 적으면 입력 지연이 발생
 FPS = 120
 
-
 class FrontEnd(object):
-    """ Maintains the Tello display and moves it through the keyboard keys.
+    """ Tello 디스플레이를 유지하고 키보드 키를 통해 이동.
         Press escape key to quit.
         The controls are:
             - T: Takeoff
             - L: Land
-            - Arrow keys: Forward, backward, left and right.
-            - A and D: Counter clockwise and clockwise rotations (yaw)
-            - W and S: Up and down.
+            - 방향키: Forward, backward, left and right.
+            - A: Counter clockwise
+            - D: clockwise
+            - W: up
+            - S: down.
     """
 
     def __init__(self):
@@ -27,13 +28,12 @@ class FrontEnd(object):
         pygame.init()
 
         # pygame window창 생성
-        pygame.display.set_caption("Tello video stream")
-        self.screen = pygame.display.set_mode([960, 720])
+        pygame.display.set_caption("Tello video stream")    # 타이틀바의 텍스트를 설정
+        self.screen = pygame.display.set_mode([960, 720])   # 화면 해상도를 960*720 으로 초기화
 
-        # Init Tello object that interacts with the Tello drone
         self.tello = Tello()
 
-        # Drone velocities between -100~100
+        # 드론 속도 -100 ~ 100
         self.for_back_velocity = 0
         self.left_right_velocity = 0
         self.up_down_velocity = 0
@@ -42,23 +42,22 @@ class FrontEnd(object):
 
         self.send_rc_control = False
 
-        # create update timer
+        # 타이머를 업데이트
         pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // FPS)
 
     def run(self):
-
         self.tello.connect()
         self.tello.set_speed(self.speed)
 
-        # In case streaming is on. This happens when we quit this program without the escape key.
+        # 스트리밍이 켜져 있는 경우, 이스케이프 키 없이 이 프로그램을 종료할 때 이런 일이 발생.
         self.tello.streamoff()
         self.tello.streamon()
-        
+
         # 드론의 카메라 프레임 읽어오기
         frame_read = self.tello.get_frame_read()
 
         should_stop = False
-        while not should_stop:    # should_stop가 False라면
+        while not should_stop:  # should_stop가 False라면
 
             for event in pygame.event.get():
                 if event.type == pygame.USEREVENT + 1:
@@ -76,59 +75,59 @@ class FrontEnd(object):
             if frame_read.stopped:
                 break
 
-            self.screen.fill([0, 0, 0])
+            self.screen.fill([0, 0, 0])     # 화면을 지우기 위해 호출(검정색)
 
             frame = frame_read.frame
             text = "Battery: {}%".format(self.tello.get_battery())
             cv2.putText(frame, text, (5, 720 - 5),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame = np.rot90(frame)
             frame = np.flipud(frame)
 
             frame = pygame.surfarray.make_surface(frame)
-            self.screen.blit(frame, (0, 0))
+            self.screen.blit(frame, (0, 0))     # 지정한 좌표가 이미지의 왼쪽 위에 위치하도록 출력
             pygame.display.update()
 
             time.sleep(1 / FPS)
 
-        # Call it always before finishing. To deallocate resources.
+        # 종료할 때 항상 호출, 리소스 회수
         self.tello.end()
 
     def keydown(self, key):
-        """ Update velocities based on key pressed
+        """ 키가 눌렸을 경우
         Arguments:
             key: pygame key
         """
-        if key == pygame.K_UP:  # set forward velocity
+        if key == pygame.K_UP:  # forward
             self.for_back_velocity = S
-        elif key == pygame.K_DOWN:  # set backward velocity
+        elif key == pygame.K_DOWN:  # backward
             self.for_back_velocity = -S
-        elif key == pygame.K_LEFT:  # set left velocity
+        elif key == pygame.K_LEFT:  # left
             self.left_right_velocity = -S
-        elif key == pygame.K_RIGHT:  # set right velocity
+        elif key == pygame.K_RIGHT:  # right
             self.left_right_velocity = S
-        elif key == pygame.K_w:  # set up velocity
+        elif key == pygame.K_w:  # up
             self.up_down_velocity = S
-        elif key == pygame.K_s:  # set down velocity
+        elif key == pygame.K_s:  # down
             self.up_down_velocity = -S
-        elif key == pygame.K_a:  # set yaw counter clockwise velocity
+        elif key == pygame.K_a:  # counter clockwise
             self.yaw_velocity = -S
-        elif key == pygame.K_d:  # set yaw clockwise velocity
+        elif key == pygame.K_d:  # clockwise
             self.yaw_velocity = S
 
     def keyup(self, key):
-        """ Update velocities based on key released
+        """ 키를 안누른 상태
         Arguments:
             key: pygame key
         """
-        if key == pygame.K_UP or key == pygame.K_DOWN:  # set zero forward/backward velocity
+        if key == pygame.K_UP or key == pygame.K_DOWN:  # set zero forward/backward
             self.for_back_velocity = 0
-        elif key == pygame.K_LEFT or key == pygame.K_RIGHT:  # set zero left/right velocity
+        elif key == pygame.K_LEFT or key == pygame.K_RIGHT:  # set zero left/right
             self.left_right_velocity = 0
-        elif key == pygame.K_w or key == pygame.K_s:  # set zero up/down velocity
+        elif key == pygame.K_w or key == pygame.K_s:  # set zero up/down
             self.up_down_velocity = 0
-        elif key == pygame.K_a or key == pygame.K_d:  # set zero yaw velocity
+        elif key == pygame.K_a or key == pygame.K_d:  # set zero yaw
             self.yaw_velocity = 0
         elif key == pygame.K_t:  # takeoff
             self.tello.takeoff()
@@ -138,10 +137,10 @@ class FrontEnd(object):
             self.send_rc_control = False
 
     def update(self):
-        """ Update routine. Send velocities to Tello."""
+        """ 드론에게 전달할 명령어 업데이트 """
         if self.send_rc_control:
             self.tello.send_rc_control(self.left_right_velocity, self.for_back_velocity,
-                self.up_down_velocity, self.yaw_velocity)
+                                       self.up_down_velocity, self.yaw_velocity)
 
 
 def main():
